1079f496a6803e4075014250d55fcd512861448a
Szymon Baczynski <simon.baczynski@gmail.com>
2019-04-22 22:45:42 +0200

    update 1

--- ESP_ThermalCamera_WebServer.ino
+++ ESP_ThermalCamera_WebServer.ino
@@ -1,9 +1,320 @@
-void setup() {
-  // put your setup code here, to run once:
+/*
+  Read the temperature pixels from the MLX90640 IR array
+  By: Nathan Seidle
+  SparkFun Electronics
+  Date: May 22nd, 2018
+  License: MIT. See license file for more information but you can
+  basically do whatever you want with this code.
 
+  Feel like supporting open source hardware?
+  Buy a board from SparkFun! https://www.sparkfun.com/products/14769
+
+  This example initializes the MLX90640 and outputs the 768 temperature values
+  from the 768 pixels.
+
+  This example will work with a Teensy 3.1 and above. The MLX90640 requires some
+  hefty calculations and larger arrays. You will need a microcontroller with 20,000
+  bytes or more of RAM.
+
+  This relies on the driver written by Melexis and can be found at:
+  https://github.com/melexis/mlx90640-library
+
+  Hardware Connections:
+  Connect the SparkFun Qwiic Breadboard Jumper (https://www.sparkfun.com/products/14425)
+  to the Qwiic board
+  Connect the male pins to the Teensy. The pinouts can be found here: https://www.pjrc.com/teensy/pinout.html
+  Open the serial monitor at 9600 baud to see the output
+*/
+
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1331.h>
+
+
+#include "MLX90640_API.h"
+#include "MLX90640_I2C_Driver.h"
+
+const byte MLX90640_address = 0x33; //Default 7-bit unshifted address of the MLX90640
+
+#define TA_SHIFT 8 //Default shift for MLX90640 in open air
+
+float mlx90640To[768];
+paramsMLX90640 mlx90640;
+
+// You can use any (4 or) 5 pins
+#define sclk 18
+#define mosi 23
+#define cs   17
+#define rst  5
+#define dc   16
+
+
+// Color definitions
+#define BLACK           0x0000
+#define BLUE            0x001F
+#define RED             0xF800
+#define GREEN           0x07E0
+#define CYAN            0x07FF
+#define MAGENTA         0xF81F
+#define YELLOW          0xFFE0
+#define WHITE           0xFFFF
+
+//the colors we will be using
+const uint16_t camColors[] = {0x480F,
+                              0x400F, 0x400F, 0x400F, 0x4010, 0x3810, 0x3810, 0x3810, 0x3810, 0x3010, 0x3010,
+                              0x3010, 0x2810, 0x2810, 0x2810, 0x2810, 0x2010, 0x2010, 0x2010, 0x1810, 0x1810,
+                              0x1811, 0x1811, 0x1011, 0x1011, 0x1011, 0x0811, 0x0811, 0x0811, 0x0011, 0x0011,
+                              0x0011, 0x0011, 0x0011, 0x0031, 0x0031, 0x0051, 0x0072, 0x0072, 0x0092, 0x00B2,
+                              0x00B2, 0x00D2, 0x00F2, 0x00F2, 0x0112, 0x0132, 0x0152, 0x0152, 0x0172, 0x0192,
+                              0x0192, 0x01B2, 0x01D2, 0x01F3, 0x01F3, 0x0213, 0x0233, 0x0253, 0x0253, 0x0273,
+                              0x0293, 0x02B3, 0x02D3, 0x02D3, 0x02F3, 0x0313, 0x0333, 0x0333, 0x0353, 0x0373,
+                              0x0394, 0x03B4, 0x03D4, 0x03D4, 0x03F4, 0x0414, 0x0434, 0x0454, 0x0474, 0x0474,
+                              0x0494, 0x04B4, 0x04D4, 0x04F4, 0x0514, 0x0534, 0x0534, 0x0554, 0x0554, 0x0574,
+                              0x0574, 0x0573, 0x0573, 0x0573, 0x0572, 0x0572, 0x0572, 0x0571, 0x0591, 0x0591,
+                              0x0590, 0x0590, 0x058F, 0x058F, 0x058F, 0x058E, 0x05AE, 0x05AE, 0x05AD, 0x05AD,
+                              0x05AD, 0x05AC, 0x05AC, 0x05AB, 0x05CB, 0x05CB, 0x05CA, 0x05CA, 0x05CA, 0x05C9,
+                              0x05C9, 0x05C8, 0x05E8, 0x05E8, 0x05E7, 0x05E7, 0x05E6, 0x05E6, 0x05E6, 0x05E5,
+                              0x05E5, 0x0604, 0x0604, 0x0604, 0x0603, 0x0603, 0x0602, 0x0602, 0x0601, 0x0621,
+                              0x0621, 0x0620, 0x0620, 0x0620, 0x0620, 0x0E20, 0x0E20, 0x0E40, 0x1640, 0x1640,
+                              0x1E40, 0x1E40, 0x2640, 0x2640, 0x2E40, 0x2E60, 0x3660, 0x3660, 0x3E60, 0x3E60,
+                              0x3E60, 0x4660, 0x4660, 0x4E60, 0x4E80, 0x5680, 0x5680, 0x5E80, 0x5E80, 0x6680,
+                              0x6680, 0x6E80, 0x6EA0, 0x76A0, 0x76A0, 0x7EA0, 0x7EA0, 0x86A0, 0x86A0, 0x8EA0,
+                              0x8EC0, 0x96C0, 0x96C0, 0x9EC0, 0x9EC0, 0xA6C0, 0xAEC0, 0xAEC0, 0xB6E0, 0xB6E0,
+                              0xBEE0, 0xBEE0, 0xC6E0, 0xC6E0, 0xCEE0, 0xCEE0, 0xD6E0, 0xD700, 0xDF00, 0xDEE0,
+                              0xDEC0, 0xDEA0, 0xDE80, 0xDE80, 0xE660, 0xE640, 0xE620, 0xE600, 0xE5E0, 0xE5C0,
+                              0xE5A0, 0xE580, 0xE560, 0xE540, 0xE520, 0xE500, 0xE4E0, 0xE4C0, 0xE4A0, 0xE480,
+                              0xE460, 0xEC40, 0xEC20, 0xEC00, 0xEBE0, 0xEBC0, 0xEBA0, 0xEB80, 0xEB60, 0xEB40,
+                              0xEB20, 0xEB00, 0xEAE0, 0xEAC0, 0xEAA0, 0xEA80, 0xEA60, 0xEA40, 0xF220, 0xF200,
+                              0xF1E0, 0xF1C0, 0xF1A0, 0xF180, 0xF160, 0xF140, 0xF100, 0xF0E0, 0xF0C0, 0xF0A0,
+                              0xF080, 0xF060, 0xF040, 0xF020, 0xF800,
+                             };
+
+
+float p = 3.1415926;
+Adafruit_SSD1331 display = Adafruit_SSD1331(cs, dc, mosi, sclk, rst);
+//Adafruit_SSD1331 display = Adafruit_SSD1331(&SPI, cs, dc, rst);
+
+void setup()
+{
+  Wire.begin();
+  Wire.setClock(400000); //Increase I2C clock speed to 400kHz
+  Serial.begin(115200);while (!Serial); //Wait for user to open terminal
+  
+  Serial.print("SDA pin: "); Serial.println(SDA);
+  Serial.print("SCL pin: ");Serial.println(SCL);
+
+  
+  Serial.println("MLX90640 IR Array Example");
+
+  if (isConnected() == false)
+  {
+    Serial.println("MLX90640 not detected at default I2C address. Please check wiring. Freezing.");
+    while (1);
+  }
+  Serial.println("MLX90640 online!");
+
+  //Get device parameters - We only have to do this once
+  int status=0;
+  uint16_t eeMLX90640[832];
+  status = MLX90640_DumpEE(MLX90640_address, eeMLX90640);
+  if (status != 0)
+    Serial.println("Failed to load system parameters");
+
+  status = MLX90640_ExtractParameters(eeMLX90640, &mlx90640);
+  if (status != 0)
+    Serial.println("Parameter extraction failed");
+  
+  int SetRefreshRate;
+  SetRefreshRate = MLX90640_SetRefreshRate(0x33,0x05);
+
+// --- Part Display OLED --- //
+
+  display.begin();
+
+  Serial.println("init");
+  uint16_t time = millis();
+  display.fillScreen(BLACK);
+  time = millis() - time;
+
+  Serial.println(time, DEC);
+  delay(500);
+
+  lcdTestPattern();
+  delay(1000);
+
+  display.fillScreen(BLACK);
+  display.setCursor(0,0);
+  display.print("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur adipiscing ante sed nibh tincidunt feugiat. Maecenas enim massa");
+  delay(1000);
+  display.fillScreen(BLACK);
+  lcdTestThermalImage();
+  delay(1000);
+  display.fillScreen(BLACK);
+  //Once params are extracted, we can release eeMLX90640 array
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+  
+  for (byte x = 0 ; x < 2 ; x++) //Read both subpages
+  {
+    uint16_t mlx90640Frame[834];
+    int status = MLX90640_GetFrameData(MLX90640_address, mlx90640Frame);
+    if (status < 0)
+    {
+      Serial.print("GetFrame Error: ");
+      Serial.println(status);
+    }
 
+    float vdd = MLX90640_GetVdd(mlx90640Frame, &mlx90640);
+    float Ta = MLX90640_GetTa(mlx90640Frame, &mlx90640);
+
+    float tr = Ta - TA_SHIFT; //Reflected temperature based on the sensor ambient temperature
+    float emissivity = 0.95;
+
+    MLX90640_CalculateTo(mlx90640Frame, &mlx90640, emissivity, tr, mlx90640To);
+
+    float MaxTemp = 0;
+    float MinTemp = 0;
+    float CenterTemp = (mlx90640To[165]+mlx90640To[180]+mlx90640To[176]+mlx90640To[192]) / 4.0;
+
+    MaxTemp = mlx90640To[0];
+    MinTemp = mlx90640To[0];
+  
+    for (int x = 0 ; x < 768 ; x++)
+    {
+      if (mlx90640To[x] > MaxTemp){
+        MaxTemp = mlx90640To[x];
+      }
+      if (mlx90640To[x] < MinTemp){
+        MinTemp = mlx90640To[x];
+      }
+    }
+    //display.fillRect(0, 0, 96, 48, BLACK);
+    lcdThermalImage(mlx90640To, MinTemp, MaxTemp);
+
+    display.fillRect(66, 0, 30, 48, BLACK);
+    
+    display.setCursor(66,0);
+    display.setTextColor(RED);
+    display.print(MaxTemp,2);
+
+    display.setCursor(66,18);
+    display.setTextColor(WHITE);
+    display.print(CenterTemp,2);
+    
+    display.setCursor(66,36);
+    display.setTextColor(BLUE);
+    display.print(MinTemp,2);
+  }
+
+ 
+  for (int x = 0 ; x < 768 ; x++)
+  {
+    //Serial.print("Pixel ");
+    Serial.print(x);
+    Serial.print(": ");
+    Serial.print(mlx90640To[x], 2);
+    //Serial.print("C");
+    Serial.println();
+  }
+
+  delay(100);
+  //display.fillScreen(BLACK);
+  
+  }
+
+//Returns true if the MLX90640 is detected on the I2C bus
+boolean isConnected()
+{
+  Wire.beginTransmission((uint8_t)MLX90640_address);
+  if (Wire.endTransmission() != 0)
+    return (false); //Sensor did not ACK
+  return (true);
 }
+
+void lcdThermalImage(float mlx90640To[], float MinTemp, float MaxTemp)
+{
+  uint8_t w,h;
+  uint8_t box = 2;
+  display.setAddrWindow(0, 0, 96, 64);
+
+  for (h = 0; h < 24; h++) {
+    for (w = 0; w < 32; w++) {
+      uint8_t colorIndex = map(mlx90640To[w+(32*h)], MinTemp, MaxTemp, 0, 255);
+      colorIndex = constrain(colorIndex, 0, 255);
+      
+      display.fillRect(box * w, box * h, box, box, camColors[colorIndex]);
+      //display.writePixel(w, h, camColors[colorIndex]);
+    }  
+  }
+  display.endWrite();
+}
+/*
+void lcdThermalImage(float mlx90640To[])
+{
+  uint8_t w,h;
+  display.setAddrWindow(0, 0, 96, 64);
+
+  for (h = 0; h < 48; h++) {
+    for (w = 0; w < 64; w++) {
+      if (h*w <= 255) {
+        display.writePixel(w, h, camColors[h*w]);
+      } else{
+        display.writePixel(w, h, RED);
+      }
+    }
+  }
+  display.endWrite();
+}
+*/
+
+void lcdTestThermalImage(void)
+{
+  uint8_t w,h;
+  display.setAddrWindow(0, 0, 96, 64);
+
+  for (h = 0; h < 48; h++) {
+    for (w = 0; w < 64; w++) {
+      if (h*w <= 255) {
+        display.writePixel(w, h, camColors[h*w]);
+      } else{
+        display.writePixel(w, h, RED);
+      }
+    }
+  }
+  display.endWrite();
+}
+
+
+void lcdTestPattern(void)
+{
+  uint8_t w,h;
+  display.setAddrWindow(0, 0, 96, 64);
+
+  for (h = 0; h < 64; h++) {
+    for (w = 0; w < 96; w++) {
+      if (w > 83) {
+        display.writePixel(w, h, WHITE);
+      } else if (w > 71) {
+        display.writePixel(w, h, BLUE);
+      } else if (w > 59) {
+        display.writePixel(w, h, GREEN);
+      } else if (w > 47) {
+        display.writePixel(w, h, CYAN);
+      } else if (w > 35) {
+        display.writePixel(w, h, RED);
+      } else if (w > 23) {
+        display.writePixel(w, h, MAGENTA);
+      } else if (w > 11) {
+        display.writePixel(w, h, YELLOW);
+      } else {
+        display.writePixel(w, h, BLACK);
+      }
+    }
+  }
+
+  display.endWrite();
+}
+
