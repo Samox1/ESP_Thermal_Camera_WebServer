93b385fb0d3f8d21768b3860da2edd3ffad296cc
Szymon Baczynski <simon.baczynski@gmail.com>
2019-10-13 16:38:47 +0200

    Up

--- ESP_ThermalCamera_WebServer.ino
+++ ESP_ThermalCamera_WebServer.ino
@@ -1,12 +1,12 @@
-/*
-Thermal Image Camera - Project using ESP8266 or ESP32 and MLX90640 sensor (32x24 px)
-Design with 0.95' OLED (SD1331) and WebServer to see images (with interpolation) on any other device
-Project based on MLX data sheet and examples
-Author: Szymon Baczyński
-Date: April 2019
-Version: Ver 1.1 
-GitHub: https://github.com/Samox1/ESP_Thermal_Camera_WebServer
-*/
+//--------------------------------------------------------------------------------------------------------//
+// Thermal Image Camera - Project using ESP8266 or ESP32 and MLX90640 sensor (32x24 px)                   //
+// Design with 0.95' OLED (SD1331) and WebServer to see images (with interpolation) on any other device   //
+// Project based on MLX data sheet and examples                                                           //
+// Author: Szymon Baczyński                                                                               //
+// Date: April 2019                                                                                       //
+// Version: Ver 1.1                                                                                       //
+// GitHub: https://github.com/Samox1/ESP_Thermal_Camera_WebServer                                         //
+//--------------------------------------------------------------------------------------------------------//
 
 #include <SPI.h>
 #include <Wire.h>
